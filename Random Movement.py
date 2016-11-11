e9b296dc957d6a824db06a06571063a5e1088e26
Alexstocky <Stockwer@uni.coventry.ac.uk>
2016-11-11 13:52:45 +0000

    Update Random Movement.py

--- Random Movement.py
+++ Random Movement.py
@@ -5,16 +5,14 @@ top = Tk()
 
 #Simple canvas with two lines to test whether the roomba will pass through walls
 canvas = Canvas(top, height=500,width=600, bg="white")
-line = canvas.create_line((150,0),(150,150), width=5)
-line = canvas.create_line((0,150),(100,150), width=5)
+line1 = canvas.create_line((150,0),(150,150),width=5)
+line2 = canvas.create_line((0,150),(100,150), width=5)
+
+wallsArray = [canvas.coords(line1), canvas.coords(line2)]
 
 canvas.pack()
 
 ###Roomba Movement
-
-#Set x and y velocities
-vx = 5.0
-vy= 5.0
 
 #Boundaries
 x_min = 0
@@ -22,11 +20,17 @@ y_min = 0
 x_max = 600
 y_max = 500
 
+def detect():
+    for n in range(0,len(wallsArray)):
+        if canvas.coords(roomba) == wallsArray[n]:
+            move = False
+
 #Create robot representation
 roomba= canvas.create_oval(0,0,15,15, fill="blue")
 
 #Generate x and y coordinates for 500 timesteps
 for t in range (1,500):
+    move = True
     x1,y1,x2,y2 = canvas.coords(roomba)
     #Make robot change direction randomly
     RandomVal=random.randint(1,100)
@@ -52,9 +56,12 @@ for t in range (1,500):
     if x1 <= x_min:
         vx = 10.0
     #Reposition the robot
-    canvas.coords(roomba,x1+vx,y1+vy,x2+vx,y2+vy)
-    canvas.update()
+    detect()
+    if move == True:
+        canvas.coords(roomba,x1+vx,y1+vy,x2+vx,y2+vy)
+        canvas.update()
     # Pause for 0.5 seconds, then delete the image
     time.sleep(0.1)
-    
+
 top.mainloop()
+
