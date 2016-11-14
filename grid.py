9a2cb52df156906bf00b8a356c4b77a8b1c8445b
Alexstocky <Stockwer@uni.coventry.ac.uk>
2016-11-14 14:13:33 +0000

    Added functions
    Two functions to simplify use

--- grid.py
+++ grid.py
@@ -1,17 +1,34 @@
 import random
 
 class grid:
+
+    #Simplifies the process of creating walls by creating lists of coordinates along a given line upto a given point. Needs only be called within grid.__init__ function.
+    #horv is a bool where True creates a horizontal line and False a vertical line, along is the x or y coord the line is to be based on. upto and start represent the start and end of the line.
+    def genImpasslist(horv, along, upto, start=0):
+        outlist = []
+        if horv:
+            for x in range(start,upto):
+                outlist.append([along, x])
+        else:
+            for y in range(start,upto):
+                outlist.append([y, along])
+        return outlist
+        
+    def addImpasses(self, impasses):
+        self.impasses += impasses
+        for x in impasses:
+             self.grid[x[0]][x[1]] ['Impass'] = True
+    
     def __init__(self,height,width,numDirt,impasses=[]): #Generates a tuple of tuples of dictionaries representing the 'gamespace'. Height is the number of tuples containing dictionaries. Width is the number of dictionaries in each tuple.
         self.grid = []                                   #Impasses is a list of coordinates of place that are inpassable. Spawn right now only only contains the start position of doggo, but will contain roomba at some point. numDirt is the number of dirts to be placed
-        self.impasses = impasses
+        self.impasses = []
         for h in range(height):
             self.grid.append([])
             for w in range(width):
                 self.grid[h].append({'Impass':False, 'Doggo':False})
             tuple(self.grid[h])
         tuple(self.grid)
-        for x in impasses:
-             self.grid[x[0]][x[1]] ['Impass'] = True
+        self.addImpasses(impasses)
         i = 0
         for x in self.grid:
             for y in x:
