4c355f6e337564c57cde9c750407d50824c4b074
Djdodge <george.georgiev.simeonov@gmail.com>
2016-11-24 14:45:12 +0000

    Working chase dirt function
    Still have to add wall detection and dog

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import random
+import time
 # GRID DICTIONARY :
+# WHEN USING GRID[][] FIRST ONE IS Y YOU S!%@#
 # 0 - Free space
 # 1 - Impassable Object eg. wall/table/chair
 # 2 - Dirt
@@ -8,6 +10,9 @@ class Grid():   #This generates the empty grid #INCLUDES THE WALLS AROUND THE GR
     def __init__(self,xSize,ySize):
         self.xSize = xSize
         self.ySize = ySize
+    def testout(self,x,y):
+        grid1[x][y]=4
+        self.PrintGrid(grid1)
     def Generate(self):   
         y = []
         i = 0
@@ -27,14 +32,18 @@ class Grid():   #This generates the empty grid #INCLUDES THE WALLS AROUND THE GR
         return y
     def PlaceDirt(self,amount):
         i=0
+        self.dirtx = []
+        self.dirty = []
         while(i<amount):
-            randx = random.randint(0,self.xSize-1)
-            randy = random.randint(0,self.ySize-1)
+            randx = random.randint(0,self.xSize)
+            randy = random.randint(0,self.ySize)
             try:
-                if(grid1[randx][randy] == 0 and random.random() <((amount-i)/mapsize)):
-                    grid1[randx][randy] = 2
+                if(grid1[randy][randx] == 0 and random.random() <((amount-i)/mapsize)):
+                    grid1[randy][randx] = 2
+                    self.dirtx.append(randx)
+                    self.dirty.append(randy)
                     i+=1
-                    canvas.create_rectangle(randx*10, randy*10, randx*10 + 10, randy*10+ 10, fill="brown")
+                    canvas.create_rectangle(randx*10, randy*10, randx*10 + 10, randy*10+ 10, fill="red")
             except IndexError:
                 continue
     def PrintGrid(self,grid):
@@ -42,28 +51,7 @@ class Grid():   #This generates the empty grid #INCLUDES THE WALLS AROUND THE GR
         while(i<self.ySize):
             print(grid[i])
             i+=1
-##    def PlaceWalls(self,grid):
-##        i = 1
-##        while i in range(self.ySize):
-##            z = 1
-##            while z in range(self.xSize):
-##                if(z<=self.xSize//2 and i<=self.ySize//2):   # Determine Quadrant to figure out direction wich wall should be face
-##                    quadrant = 1                                       # Quadrants :
-##                elif(z>self.xSize//2 and i<=self.ySize//2):  #  1  2
-##                    quadrant = 2                             #  3  4
-##                elif(z<=self.xSize//2 and i>self.ySize//2):
-##                    quadrant = 3
-##                else:
-##                    quadrant = 4
-##            if(quadrant=1):
-##                it=0
-##                maxWallSize = random.random(1,5)
-##                direction = random.random(1,2)
-##                while(it<maxWallSize):
-##                    if(direction==1):
-##                        grid[i][z+it]=1
-##                    else:
-##                        grid 
+            
     def PlaceWalls(self,grid):
         i = 1
         while i in range(self.ySize):
@@ -84,25 +72,63 @@ class Grid():   #This generates the empty grid #INCLUDES THE WALLS AROUND THE GR
             i+=2
     def GetySize(self):
         return self.ySize
+    def GetxSize(self):
+        return self.xSize
 
 class Roomba():
     def __init__(self,grid,gridobj):
+        self.index = 0
         while True:
-            roombaCoordx = random.randint(0,gridobj.GetySize()//4)
+            roombaCoordx = random.randint(0,gridobj.GetxSize()//4)
             roombaCoordy= random.randint(0,gridobj.GetySize()//4)
-            if(grid[roombaCoordx][roombaCoordy] == 0):
-                canvas.create_oval(roombaCoordx*10,roombaCoordy*10,roombaCoordx*10 + 12,roombaCoordy*10 + 12,fill="blue")
-                grid[roombaCoordx][roombaCoordy] = 'R'
-                self.rX = roombaCoordx
-                self.rY = roombaCoordy
+            if(grid[roombaCoordy][roombaCoordx] == 0):
+                self.roomba = canvas.create_rectangle(roombaCoordx*10 , roombaCoordy*10,roombaCoordx*10 + 10,roombaCoordy*10 + 10,fill="green")
+                grid[roombaCoordy][roombaCoordx] = 'R'
+                self.rx = roombaCoordx
+                self.ry = roombaCoordy
                 break
-    #def ChaseDirt(self):
-        
+    def ChaseDirt(self,grid,gridobj):
+        time.sleep(0.01)
+        canvas.delete(self.roomba)
+        print("ry",self.ry,"rx",self.rx,"DIRTY",gridobj.dirty[self.index],"DIRTX",gridobj.dirtx[self.index], " INDEX " , self.index)
+        if(self.ry < gridobj.dirty[self.index] and self.rx < gridobj.dirtx[self.index]):
+            grid[self.ry][self.rx]=0
+            self.ry+=1
+            self.rx+=1
+        elif(self.ry < gridobj.dirty[self.index] and self.rx > gridobj.dirtx[self.index]):
+            grid[self.ry][self.rx]=0
+            self.ry+=1
+            self.rx-=1
+        elif(self.ry > gridobj.dirty[self.index] and self.rx < gridobj.dirtx[self.index]):
+            grid[self.ry][self.rx]=0
+            self.ry-=1
+            self.rx+=1
+        elif(self.ry > gridobj.dirty[self.index] and self.rx > gridobj.dirtx[self.index]):
+            grid[self.ry][self.rx]=0
+            self.ry-=1
+            self.rx-=1
+        elif(self.ry == gridobj.dirty[self.index] and self.rx < gridobj.dirtx[self.index]):
+            grid[self.ry][self.rx]=0
+            self.rx+=1
+        elif(self.ry == gridobj.dirty[self.index] and self.rx > gridobj.dirtx[self.index]):
+            grid[self.ry][self.rx]=0
+            self.rx-=1
+        elif(self.ry < gridobj.dirty[self.index] and self.rx == gridobj.dirtx[self.index]):
+            grid[self.ry][self.rx]=0
+            self.ry+=1
+        elif(self.ry > gridobj.dirty[self.index] and self.rx == gridobj.dirtx[self.index]):
+            grid[self.ry][self.rx]=0
+            self.ry-=1
+        else:
+            self.index+=1
+            self.roomba = canvas.create_rectangle(self.rx*10 , self.ry*10,self.rx*10 + 10,self.ry*10 + 10,fill="white")
+        grid[self.ry][self.rx]="R"
+        self.roomba = canvas.create_rectangle(self.rx*10 , self.ry*10,self.rx*10 + 10,self.ry*10 + 10,fill="blue")
 win = Tk().withdraw()
 prompt = messagebox.askyesno("hi","Do you want to add dirt manually")
 win = Tk()
-win_height=400
-win_width=400
+win_height=100
+win_width=100
 mapsize = ((win_height*win_width)//100)
 win.configure(width=win_width,height=win_height)
 canvas = Canvas(win,width=win_width,height=win_height)
@@ -111,13 +137,13 @@ grid = Grid(win_width//10,win_height//10)
 grid1 = grid.Generate()
 grid.PlaceWalls(grid1)
 if(prompt==True):
-    print("does not work")
+    print("does not work yet")
 else:
     grid.PlaceDirt(win_height//10)
 roomba = Roomba(grid1,grid)
 #Gameloop should be here
-roomba.ChaseDirt()
-
-
-Tk.update(win)#Delete this 
-grid.PrintGrid(grid1) # also for testing delete
+while True:
+    roomba.ChaseDirt(grid1,grid)
+    Tk.update(win)
+    grid.PrintGrid(grid1)
+#Gameloop end
